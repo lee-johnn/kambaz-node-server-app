d6cdfecf1e70d217cd75f9300174c00c3c7a900b
leejohnnn <lee.joh@northeastern.edu>
2025-04-15 01:37:30 -0400

    a6

--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -1,7 +1,15 @@
-import * as dao from "./dao.js";
+import * as enrollmentsDao from "./dao.js";
 
-export default function EnrollmentRoutes(app) {
-  app.get("/api/enrollments", (req, res) => {
-    res.json(dao.getAllEnrollments());
-  });
+export default function EnrollmentsRoutes(app) {
+    app.post("/api/enrollments/:courseId/:userId", async (req, res) => {
+        const { courseId, userId } = req.params;
+        const status = await enrollmentsDao.enrollUserInCourse(userId, courseId);
+        res.send(status);
+    });
+    
+    app.delete("/api/enrollments/:courseId/:userId", async (req, res) => {
+        const { courseId, userId } = req.params;
+        const status = await enrollmentsDao.unenrollUserFromCourse(userId, courseId);
+        res.send(status);
+    });
 }
